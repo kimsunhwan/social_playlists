59405f524eae6f5d58656c4b5ba1435ff3a89887
Calvin Tuong <calvin.tuong@gmail.com>
2012-05-27 15:10:34 -0700

    added code for adding videos to playlists. needs testing

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -24,6 +24,26 @@ class PlaylistsController < ApplicationController
   def add_video_to_playlist
     puts params
     playlist = Playlist.find(params[:playlistId])
+    video = Video.find_by_site_code(params[:videoId]).first
+    newPosition = params[:newPosition]
+
+    if !video then
+      video = Video.new(:name => params[:name], :length => params[:length], 
+        :upvotes => 0, :downvotes => 0, :type_id => 1, :views => 0, :site_code => params[:videoId])
+      playlist.videos << video
+      video.save
+      playlist.save
+    end
+
+    playlist.orderings.each do |o|
+      if o.video_id == video.id then
+        o.order = newPosition
+        o.save
+      elsif o.order >= newPosition then
+        o.order += 1
+        o.save
+      end  
+    end
     
     render :json => nil
   end
